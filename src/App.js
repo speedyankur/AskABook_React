e84757689a94dc2f2e24f644e2b212acc2a0d904
Ankur Garha <a65379@eon.com>
2023-03-31 13:45:25 +0200

    feat: updated the BG image and logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Dictaphone from "./components/dictaphone/dictaphone";
+import logo from "./bg.jpg";
 function App() {
   return (
     <div className="App">
+      <img src={logo} width="100%"></img>
+      <h1>ALL ABOUT PREMATURE BABIES</h1>
       <Dictaphone />
     </div>
   );
